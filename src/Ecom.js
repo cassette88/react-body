3067f366daf4213ed11c43b6d59ad1da368b607a
Dave <chopin@iJones.local>
2019-06-24 16:13:09 -0400

    added nav from rbx

--- src/Ecom.js
+++ src/Ecom.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react'
-import styled from '@emotion/styled'
+import React, { Component } from 'react';
+import styled from '@emotion/styled';
+import Nav from './Navbar'
 
 
 const Button = styled.button`
+  padding: 4em;
+  margin: 2em;
+  font-size:1em;
   color: ${props =>
     props.primary ? 'hotpink' : 'turquoise'};
 `
 
 const Container = styled.div(props => ({
   display: 'flex',
+  marginLeft: '3em',
+  marginRight: '3em',
   flexDirection: props.column && 'column'
 }))
 
@@ -18,9 +24,10 @@ export default class Ecom extends Component {
     render() {
         return (
             <div>
+            <Nav />
             <Container column>
-            <Button>This is a regular button.</Button>
-            <Button primary>This is a primary button.</Button>
+            <Button>Regular</Button>
+            <Button primary>Primary</Button>
           </Container>
           </div>
         )
