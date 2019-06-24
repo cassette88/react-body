5b1d3fa23f9b56b65df4b5bdf3fbd91b034cd2d8
Dave <chopin@iJones.local>
2019-06-24 19:21:48 -0400

    added rebass/emotion

--- src/Ecom.js
+++ src/Ecom.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Image } from '@rebass/emotion';
 import styled from '@emotion/styled';
-import Nav from './Navbar'
-import Foot from './Footer'
+import Nav from './Navbar';
+import Foot from './Footer';
 
 
 const Button = styled.button`
+ text-align: center;
   padding: 4em;
   margin: 2em;
   font-size:1em;
@@ -13,10 +15,11 @@ const Button = styled.button`
 `
 
 const Container = styled.div(props => ({
-  position: 'relative',
+  // position: 'relative',
   display: 'flex',
   marginLeft: '3em',
   marginRight: '3em',
+  marginBottom: '3em',
   flexDirection: props.column && 'column'
 }))
 
@@ -25,15 +28,23 @@ const Container = styled.div(props => ({
 export default class Ecom extends Component {
     render() {
         return (
-            <div>
-            <Nav />
-            <Container column>
+          <div>
+             <Nav />
+          <Container column>
+              
             <Button>Regular</Button>
             <Button primary>Primary</Button>
+            </Container>
+
+            <Container column>
+              <Button>Regular</Button>
+            <Button primary>Primary</Button>
+            <Image
+  src='https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20'
+/>
            </Container>
-           <Foot />
-         
-          </div>
+         <Foot />
+         </div>
         )
     }
 }
--- src/Footer.js
+++ src/Footer.js
@@ -6,11 +6,13 @@ import { Footer, Content } from "rbx";
 
 export default function Foot() {
     var divStyle = {
-        position: 'absolute',
+        // position: 'absolute',
+        marginTop: 'auto',
         bottom: '0',
         width: '100%',
         height: '6em',
-        background: 'gray'
+        background: '#9690B6',
+       
        
 // background: '-webkit-linear-gradient(to right, #19547b, #ffd89b)',
 // background: 'linear-gradient(to right, #19547b, #ffd89b)'
